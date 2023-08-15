bb7aabde1bfeb88e3b48527959fa752681697a6a
Gabe Fires <Swicano@users.noreply.github.com>
2023-08-15 14:48:05 -0400

    replaced HTTP status ok with adafruit equivalent

--- minfileserv.py
+++ minfileserv.py
@@ -1,10 +1,10 @@
 # Write your code here :-)
 
 import os
-
-from adafruit_httpserver import Server, MIMETypes, Request, Response, exceptions
+from adafruit_httpserver import status, exceptions
 import html
-
+import re
+import io
 
 
 def list_directory(self, path):
@@ -25,10 +25,9 @@ def list_directory(self, path):
     list.sort(key=lambda a: a.lower())
     r = []
     try:
-        displaypath = urllib.parse.unquote(self.path,
-                                           errors='surrogatepass')
+        displaypath = urllib_unquote(self.path, errors='surrogatepass')
     except UnicodeDecodeError:
-        displaypath = urllib.parse.unquote(self.path)
+        displaypath = urllib_unquote(self.path)
     displaypath = html.escape(displaypath, quote=False)
     enc = 'utf-8'  # sys.getfilesystemencoding()
     title = f'Directory listing for {displaypath}'
@@ -50,16 +49,185 @@ def list_directory(self, path):
             displayname = name + "@"
             # Note: a link to a directory displays with @ and links with /
         r.append('<li><a href="%s">%s</a></li>'
-                % (urllib.parse.quote(linkname,
-                                      errors='surrogatepass'),
-                   html.escape(displayname, quote=False)))
+                 % (urllib_quote(linkname, errors='surrogatepass'),
+                    html.escape(displayname, quote=False)))
     r.append('</ul>\n<hr>\n</body>\n</html>\n')
     encoded = '\n'.join(r).encode(enc, 'surrogateescape')
     f = io.BytesIO()
     f.write(encoded)
     f.seek(0)
-    self.send_response(HTTPStatus.OK)
+    self.send_response(status.Status(200, 'OK'))
     self.send_header("Content-type", "text/html; charset=%s" % enc)
     self.send_header("Content-Length", str(len(encoded)))
     self.end_headers()
     return f
+
+# # #### URLLIB quote and unquote
+
+def urllib_quote(string, safe='/', encoding=None, errors=None):
+    """quote('abc def') -> 'abc%20def'
+
+    Each part of a URL, e.g. the path info, the query, etc., has a
+    different set of reserved characters that must be quoted. The
+    quote function offers a cautious (not minimal) way to quote a
+    string for most of these parts.
+
+    RFC 3986 Uniform Resource Identifier (URI): Generic Syntax lists
+    the following (un)reserved characters.
+
+    unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
+    reserved      = gen-delims / sub-delims
+    gen-delims    = ":" / "/" / "?" / "#" / "[" / "]" / "@"
+    sub-delims    = "!" / "$" / "&" / "'" / "(" / ")"
+                  / "*" / "+" / "," / ";" / "="
+
+    Each of the reserved characters is reserved in some component of a URL,
+    but not necessarily in all of them.
+
+    The quote function %-escapes all characters that are neither in the
+    unreserved chars ("always safe") nor the additional chars set via the
+    safe arg.
+
+    The default for the safe arg is '/'. The character is reserved, but in
+    typical usage the quote function is being called on a path where the
+    existing slash characters are to be preserved.
+
+    Python 3.7 updates from using RFC 2396 to RFC 3986 to quote URL strings.
+    Now, "~" is included in the set of unreserved characters.
+
+    string and safe may be either str or bytes objects. encoding and errors
+    must not be specified if string is a bytes object.
+
+    The optional encoding and errors parameters specify how to deal with
+    non-ASCII characters, as accepted by the str.encode method.
+    By default, encoding='utf-8' (characters are encoded with UTF-8), and
+    errors='strict' (unsupported characters raise a UnicodeEncodeError).
+    """
+    if isinstance(string, str):
+        if not string:
+            return string
+        if encoding is None:
+            encoding = 'utf-8'
+        if errors is None:
+            errors = 'strict'
+        string = string.encode(encoding, errors)
+    else:
+        if encoding is not None:
+            raise TypeError("quote() doesn't support 'encoding' for bytes")
+        if errors is not None:
+            raise TypeError("quote() doesn't support 'errors' for bytes")
+    return quote_from_bytes(string, safe)
+
+
+_ALWAYS_SAFE = frozenset(b'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                         b'abcdefghijklmnopqrstuvwxyz'
+                         b'0123456789'
+                         b'_.-~')
+_ALWAYS_SAFE_BYTES = bytes(_ALWAYS_SAFE)
+
+def quote_from_bytes(bs, safe='/'):
+    """Like quote(), but accepts a bytes object rather than a str, and does
+    not perform string-to-bytes encoding.  It always returns an ASCII string.
+    quote_from_bytes(b'abc def\x3f') -> 'abc%20def%3f'
+    """
+    if not isinstance(bs, (bytes, bytearray)):
+        raise TypeError("quote_from_bytes() expected bytes")
+    if not bs:
+        return ''
+    if isinstance(safe, str):
+        # Normalize 'safe' by converting to bytes and removing non-ASCII chars
+        safe = safe.encode('ascii', 'ignore')
+    else:
+        # List comprehensions are faster than generator expressions.
+        safe = bytes([c for c in safe if c < 128])
+    if not bs.rstrip(_ALWAYS_SAFE_BYTES + safe):
+        return bs.decode()
+    quoter = _byte_quoter_factory(safe)
+    return ''.join([quoter(char) for char in bs])
+
+def _byte_quoter_factory(safe):
+    return _Quoter(safe).__getitem__
+
+class _Quoter(dict):
+    """A mapping from bytes numbers (in range(0,256)) to strings.
+
+    String values are percent-encoded byte values, unless the key < 128, and
+    in either of the specified safe set, or the always safe set.
+    """
+    # Keeps a cache internally, via __missing__, for efficiency (lookups
+    # of cached keys don't call Python code at all).
+    def __init__(self, safe):
+        """safe: bytes object."""
+        self.safe = _ALWAYS_SAFE.union(safe)
+
+    def __repr__(self):
+        return f"<Quoter {dict(self)!r}>"
+
+    def __missing__(self, b):
+        # Handle a cache miss. Store quoted string in cache and return.
+        res = chr(b) if b in self.safe else '%{:02X}'.format(b)
+        self[b] = res
+        return res
+
+
+_hexdig = '0123456789ABCDEFabcdef'
+_hextobyte = None
+
+def unquote_to_bytes(string):
+    """unquote_to_bytes('abc%20def') -> b'abc def'."""
+    # Note: strings are encoded as UTF-8. This is only an issue if it contains
+    # unescaped non-ASCII characters, which URIs should not.
+    if not string:
+        # Is it a string-like object?
+        string.split
+        return b''
+    if isinstance(string, str):
+        string = string.encode('utf-8')
+    bits = string.split(b'%')
+    if len(bits) == 1:
+        return string
+    res = [bits[0]]
+    append = res.append
+    # Delay the initialization of the table to not waste memory
+    # if the function is never called
+    global _hextobyte
+    if _hextobyte is None:
+        _hextobyte = {(a + b).encode(): bytes.fromhex(a + b)
+                      for a in _hexdig for b in _hexdig}
+    for item in bits[1:]:
+        try:
+            append(_hextobyte[item[:2]])
+            append(item[2:])
+        except KeyError:
+            append(b'%')
+            append(item)
+    return b''.join(res)
+
+_asciire = re.compile('([\x00-\x7f]+)')
+
+def urllib_unquote(string, encoding='utf-8', errors='replace'):
+    """Replace %xx escapes by their single-character equivalent. The optional
+    encoding and errors parameters specify how to decode percent-encoded
+    sequences into Unicode characters, as accepted by the bytes.decode()
+    method.
+    By default, percent-encoded sequences are decoded with UTF-8, and invalid
+    sequences are replaced by a placeholder character.
+
+    unquote('abc%20def') -> 'abc def'.
+    """
+    if isinstance(string, bytes):
+        return unquote_to_bytes(string).decode(encoding, errors)
+    if '%' not in string:
+        string.split
+        return string
+    if encoding is None:
+        encoding = 'utf-8'
+    if errors is None:
+        errors = 'replace'
+    bits = _asciire.split(string)
+    res = [bits[0]]
+    append = res.append
+    for i in range(1, len(bits), 2):
+        append(unquote_to_bytes(bits[i]).decode(encoding, errors))
+        append(bits[i + 1])
+    return ''.join(res)
